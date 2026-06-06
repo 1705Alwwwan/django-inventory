79845cd2b6333c94a64617229ed71a0dc9dd1d5c
Alwan Farhan Aziz <alwwwan.farhan@gmail.com>
2026-06-06 17:55:11 +0700

    Initial Django project

---
+++ app/inventory/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=200)
+
+    stock = models.IntegerField(default=0)
+
+    price = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+
+    image = models.ImageField(
+        upload_to='products/',
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
